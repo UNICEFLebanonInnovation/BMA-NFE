fde85c7c600431b3147a8787f90b6e3264c05c2d
hajjm <107342908+hajjm@users.noreply.github.com>
2026-03-30 00:29:17 +0300

    bug fixes
    caching while registration
    birthdate probelm

--- student_registration/static/js/mscc/mscc.js
+++ student_registration/static/js/mscc/mscc.js
@@ -1,4 +1,5 @@
 
+
 var arabic_fields = "#id_child_first_name, #id_child_father_name, #id_child_last_name, #id_child_mother_fullname, " +
     " #id_caregiver_mother_name, #id_caregiver_last_name, #id_caregiver_middle_name, #id_caregiver_first_name";
 
@@ -720,15 +721,26 @@ function validateMainForm(showModal, step) {
     });
 
     // Date validation
-    var year = parseInt($('#id_child_birthday_year').val()) || 0;
-    var month = parseInt($('#id_child_birthday_month').val()) || 0;
-    var day = parseInt($('#id_child_birthday_day').val()) || 0;
+    var year = parseInt($('#id_child_birthday_year').val(), 10) || 0;
+    var month = parseInt($('#id_child_birthday_month').val(), 10) || 0;
+    var day = parseInt($('#id_child_birthday_day').val(), 10) || 0;
+
     if (year && month && day) {
         var dt = new Date(year, month - 1, day);
-        if (dt.getFullYear() !== year || dt.getMonth() !== month - 1 || dt.getDate() !== day) {
-            showError('#id_child_birthday_year', 'The date is not valid.');
+
+    if (dt.getFullYear() !== year || dt.getMonth() !== month - 1 || dt.getDate() !== day) {
+        showError('#id_child_birthday_year', 'The date is not valid.');
+        valid = false;
+    } else {
+        var today = new Date();
+        today.setHours(0, 0, 0, 0);
+        dt.setHours(0, 0, 0, 0);
+
+        if (dt > today) {
+            showError('#id_child_birthday_year', 'Birth date cannot be after today.');
             valid = false;
         }
+    }
     } else {
         if (!year) showError('#id_child_birthday_year', 'This field is required');
         if (!month) showError('#id_child_birthday_month', 'This field is required');
