b6b3f81550edf7807399187ea21dabd1624bcddb
hajjm <107342908+hajjm@users.noreply.github.com>
2026-03-30 00:16:44 +0300

    attendance export fix

--- student_registration/attendances/urls.py
+++ student_registration/attendances/urls.py
@@ -1,46 +1,18 @@
 from __future__ import absolute_import, unicode_literals
-
 from django.urls import re_path
-
-from . import views
+from . import export_views
 
 app_name = 'attendances'
 
 urlpatterns = [
-    # re_path(
-    #     r'^attendance-absence/$',
-    #     view=views.AttendanceAbsenceView.as_view(),
-    #     name='attendance_absence'
-    # ),
-    # re_path(
-    #     r'^absence-export/(?P<number_of_absences>[\w.@+-]+)/(?P<total_days>[\w.@+-]+)/$',
-    #     view=views.absence_export,
-    #     name='absence_export'
-    # ),
-    # re_path(
-    #     r'^attendance-export/(?P<month>[\w.@+-]+)/(?P<year>[\w.@+-]+)/$',
-    #     view=views.attendance_export,
-    #     name='attendance_export'
-    # ),
-    # re_path(
-    #     r'^total-attendance-export/(?P<round>[\w.@+-]+)/$',
-    #     view=views.total_attendance_export,
-    #     name='total_attendance_export'
-    # ),
-    # re_path(
-    #     r'^consecutive-attendance-export/(?P<round>[\w.@+-]+)/$',
-    #     view=views.consecutive_absence_export,
-    #     name='consecutive_attendance_export'
-    # ),
-    # re_path(
-    #     r'^mscc-attendance-export/(?P<month>[\w.@+-]+)/(?P<year>[\w.@+-]+)/(?P<education_program>[\w\s.@+-]*)/(?P<class_section>[\w\s.@+-]*)/(?P<partner>[\w\s.@+-]*)/(?P<center>[\w\s.@+-]*)/?$',
-    #     view=views.mscc_attendance_export,
-    #     name='mscc_attendance_export'
-    # ),
-    # re_path(
-    #     r'^mscc-total-attendance-export/(?P<month>[\w.@+-]+)/(?P<year>[\w.@+-]+)/(?P<education_program>[\w\s.@+-]*)/(?P<class_section>[\w\s.@+-]*)/(?P<partner>[\w\s.@+-]*)/(?P<center>[\w\s.@+-]*)/?$',
-    #     view=views.mscc_total_attendance_export,
-    #     name='mscc_total_attendance_export'
-    # ),
-
+    re_path(
+        r'^mscc-attendance-export/(?P<month>[\w.@+-]+)/(?P<year>[\w.@+-]+)/(?P<education_program>[\w\s.@+-]*)/(?P<class_section>[\w\s.@+-]*)/(?P<partner>[\w\s.@+-]*)/(?P<center>[\w\s.@+-]*)/?$',
+        view=export_views.mscc_attendance_export,
+        name='mscc_attendance_export'
+    ),
+    re_path(
+        r'^mscc-total-attendance-export/(?P<month>[\w.@+-]+)/(?P<year>[\w.@+-]+)/(?P<education_program>[\w\s.@+-]*)/(?P<class_section>[\w\s.@+-]*)/(?P<partner>[\w\s.@+-]*)/(?P<center>[\w\s.@+-]*)/?$',
+        view=export_views.mscc_total_attendance_export,
+        name='mscc_total_attendance_export'
+    ),
 ]
