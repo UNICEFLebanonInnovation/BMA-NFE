dc4b0370c36b3493145592c4b56df71b891d7c69
Ali Chamseddine <achamseddine@unicef.org>
2026-05-13 11:03:53 +0300

    remove tests

--- student_registration/contrib/tests/test_smoke.py
+++
@@ -1,6 +0,0 @@
-import student_registration.contrib.sites as sites
-
-
-def test_doc_hint():
-    """Ensure the documentation hint exists."""
-    assert 'cookiecutter-django' in (sites.__doc__ or '')
